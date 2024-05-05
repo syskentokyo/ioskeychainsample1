dbcf7ea08237a52a25b250bfb0aff982083f19d3
ppengotsu <ppengotsugihub@sysken.tokyo>
2024-05-05 20:56:53 +0900

    初期プロジェクト

---
+++ ioskeychainsample1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ioskeychainsample1
+//
+//  Created by ppengotsu on 2024/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var _logLabel:UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func onTouchedButton(_ sender:Any){
+        
+    }
+
+
+}
+
