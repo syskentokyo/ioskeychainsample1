11938266bbfb81cbfc1e05b35afdfb6e2e5648d2
ppengotsu <ppengotsugihub@sysken.tokyo>
2024-05-07 00:33:57 +0900

    とりあえず保存と読み込みができる

--- ioskeychainsample1/ViewController.swift
+++ ioskeychainsample1/ViewController.swift
@@ -10,17 +10,50 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var _logLabel:UILabel!
+    @IBOutlet weak var _inputField:UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    
-    
-    @IBAction func onTouchedButton(_ sender:Any){
         
+        
+        
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        _inputField.text=dateFormatter.string(from: date)
+    }
+    
+    
+    @IBAction func onTouchedSaveButton(_ sender:Any){
+        let customKeychainManager = CustomKeychianManager()
+        
+        let serviceName = Bundle.main.bundleIdentifier! ;
+        
+        print("保存します")
+        customKeychainManager.save(serviceName: serviceName, keyName: "test2", saveValue: _inputField.text!)
+    }
+    
+    @IBAction func onTouchedReadButton(_ sender:Any){
+        let customKeychainManager = CustomKeychianManager()
+        
+        let serviceName = Bundle.main.bundleIdentifier! ;
+        
+        if(customKeychainManager.isExistData(serviceName: serviceName, keyName: "test2") == true){
+            print("読み込みます")
+            let savedTxt:String? = customKeychainManager.read(serviceName: serviceName, keyName: "test2")
+            if(savedTxt != nil){
+                _logLabel.text = savedTxt
+            }else{
+                _logLabel.text = ""
+            }
+        }
     }
 
+    @IBAction func onEndEdit(_ sender: Any) {
+        
 
+    }
+    
 }
 
